f2b5dd0ad740111d4ba0b1de6baad10be8d72cf6
Pedro Barros <pedrobars7a@gmail.com>
2021-10-14 19:14:46 -0300

    create useTheme

--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -1,15 +1,28 @@
 import React from "react";
+import { useTheme } from 'styled-components'
 
 import Header from "../Header";
 import PostsList from "../PostsList";
 import Footer from "../Footer";
 
 export default function Layout({ onToggleTheme, selectedTheme }) {
+
+  const theme = useTheme();
   return (
     <>
-      <Header  selectedTheme={selectedTheme} onToggleTheme={onToggleTheme} />
+      <Header selectedTheme={selectedTheme} onToggleTheme={onToggleTheme} />
       <PostsList />
       <Footer selectedTheme={selectedTheme} onToggleTheme={onToggleTheme} />
+
+      <div
+        style={{
+          marginTop: 24,
+          backgroundColor: theme.footerBackgroundColor,
+          padding: 24
+        }}
+      >
+        Oi, tudo bem com você?
+      </div>
     </>
   );
 }
