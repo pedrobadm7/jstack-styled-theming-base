a4a0cb1b366fc6f941a415d0663c676c4be80003
Pedro Barros <pedrobars7a@gmail.com>
2021-10-14 08:52:20 -0300

    Jstack theme class

--- src/App.js
+++ src/App.js
@@ -1,15 +1,29 @@
-import React from 'react';
+import React, { useState, useMemo } from "react";
+import { ThemeProvider } from "styled-components";
 
-import GlobalStyle from './styles/global';
-import Layout from './components/Layout';
+import GlobalStyle from "./styles/global";
+import Layout from "./components/Layout";
+
+import themes from "./styles/themes";
 
 function App() {
+  const [theme, setTheme] = useState("dark");
+
+  const currentTheme = useMemo(() => {
+    //themes[theme] = themes['dark'] = themes.theme = themes.dark
+    return themes[theme] || themes.dark;
+  }, [theme]);
+
+  const handleToggleTheme = () => {
+    setTheme((prevState) => (prevState === "dark" ? "light" : "dark"));
+  };
+
   return (
-    <>
+    <ThemeProvider theme={currentTheme}>
       <GlobalStyle />
-      <Layout />
-    </>
+      <Layout  onToggleTheme={handleToggleTheme}/>
+    </ThemeProvider>
   );
-};
+}
 
 export default App;
