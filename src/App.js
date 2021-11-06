185ba2a4ea494fcd18dc7add3b97e7d0a102b407
Pedro Barros <pedrobars7a@gmail.com>
2021-11-06 17:52:42 -0300

    componentDidMount

--- src/App.js
+++ src/App.js
@@ -7,47 +7,54 @@ import Layout from "./components/Layout";
 
 import themes from "./styles/themes";
 
-// class App extends React.Component {
-//   // constructor(props) {
-//   //   super(props);
-//   //   this.state = {
-//   //      theme: 'dark'
-//   //    };
-//   //   this.handleToggleTheme = this.handleToggleTheme.bind(this);
-//   // }
+class App extends React.Component {
+  // constructor(props) {
+  //   super(props);
+  //   this.state = {
+  //      theme: 'dark'
+  //    };
+  //   this.handleToggleTheme = this.handleToggleTheme.bind(this);
+  // }
+  state = { changed: false };
 
-//   render() {
-//     return (
-//       <ThemeProvider>
-//         <ThemeContext.Consumer>
-//           {({ theme }) => (
-//             <StyledThemeProvider theme={themes[theme] || themes.dark}>
-//               <GlobalStyle />
-//               <Layout />
-//             </StyledThemeProvider>
-//           )}
-//         </ThemeContext.Consumer>
-//       </ThemeProvider>
-//     );
-//   }
-// }
+  componentDidMount() {
+    console.log("componentDidMount executed");
+  }
 
-const App = () => {
-  const { theme } = useContext(ThemeContext);
+  render() {
+    console.log("rendered");
+    return (
+      <>
+        <button onClick={() => this.setState({ changed: true })}>
+          Change state
+        </button>
+        <ThemeContext.Consumer>
+          {({ theme }) => (
+            <StyledThemeProvider theme={themes[theme] || themes.dark}>
+              <GlobalStyle />
+              <Layout />
+            </StyledThemeProvider>
+          )}
+        </ThemeContext.Consumer>
+      </>
+    );
+  }
+}
 
-  // const [theme, setTheme] = useState("dark");
+// const App = () => {
+//   const { theme } = useContext(ThemeContext);
 
-  const currentTheme = useMemo(() => {
-    //themes[theme] = themes['dark'] = themes.theme = themes.dark
-    return themes[theme] || themes.dark;
-  }, [theme]);
+//   const currentTheme = useMemo(() => {
+//     //themes[theme] = themes['dark'] = themes.theme = themes.dark
+//     return themes[theme] || themes.dark;
+//   }, [theme]);
 
-  return (
-    <StyledThemeProvider theme={currentTheme}>
-      <GlobalStyle />
-      <Layout />
-    </StyledThemeProvider>
-  );
-};
+//   return (
+//     <StyledThemeProvider theme={currentTheme}>
+//       <GlobalStyle />
+//       <Layout />
+//     </StyledThemeProvider>
+//   );
+// };
 
 export default App;
