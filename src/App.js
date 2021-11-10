056f083ccf5f0f02b321e24269af2283756f6284
Pedro Barros <pedrobars7a@gmail.com>
2021-11-10 18:52:36 -0300

    transform themecontext in a class based component

--- src/App.js
+++ src/App.js
@@ -7,77 +7,77 @@ import Layout from "./components/Layout";
 
 import themes from "./styles/themes";
 
-class App extends React.Component {
-  // constructor(props) {
-  //   super(props);
-  //   this.state = {
-  //      theme: 'dark'
-  //    };
-  //   this.handleToggleTheme = this.handleToggleTheme.bind(this);
-  // }
-  state = { changed: false };
+// class App extends React.Component {
+//   // constructor(props) {
+//   //   super(props);
+//   //   this.state = {
+//   //      theme: 'dark'
+//   //    };
+//   //   this.handleToggleTheme = this.handleToggleTheme.bind(this);
+//   // }
+//   state = { changed: false };
 
-  componentDidMount() {
-    console.log("componentDidMount executed");
-  }
+//   componentDidMount() {
+//     console.log("componentDidMount executed");
+//   }
 
-  componentDidUpdate(prevProps, prevState) {
-    console.log("componentDidUpdate", {
-      currentState: this.state,
-      prevProps,
-      prevState,
-    });
-  }
+//   componentDidUpdate(prevProps, prevState) {
+//     console.log("componentDidUpdate", {
+//       currentState: this.state,
+//       prevProps,
+//       prevState,
+//     });
+//   }
 
-  componentDidCatch(error, info) {
-    console.log("error: ", error);
-    console.log("info: ", info);
-  }
+//   componentDidCatch(error, info) {
+//     console.log("error: ", error);
+//     console.log("info: ", info);
+//   }
 
-  shouldComponentUpdate(nextProps, nextState) {
-    console.log("shouldComponentUpdate", {
-      currentState: this.state,
-      nextProps,
-      nextState,
-    });
+//   shouldComponentUpdate(nextProps, nextState) {
+//     console.log("shouldComponentUpdate", {
+//       currentState: this.state,
+//       nextProps,
+//       nextState,
+//     });
 
-    return true;
-  }
+//     return true;
+//   }
 
-  render() {
-    console.log("rendered");
-    return (
-      <>
-        <button onClick={() => this.setState({ changed: !this.state.changed })}>
-          Change state
-        </button>
-        <ThemeContext.Consumer>
-          {({ theme }) => (
-            <StyledThemeProvider theme={themes[theme] || themes.dark}>
-              <GlobalStyle />
-              <Layout />
-            </StyledThemeProvider>
-          )}
-        </ThemeContext.Consumer>
-      </>
-    );
-  }
-}
+//   render() {
+//     console.log("rendered");
+//     return (
+//       <>
+//         <button onClick={() => this.setState({ changed: !this.state.changed })}>
+//           Change state
+//         </button>
+//         <ThemeContext.Consumer>
+//           {({ theme }) => (
+//             <StyledThemeProvider theme={themes[theme] || themes.dark}>
+//               <GlobalStyle />
+//               <Layout />
+//             </StyledThemeProvider>
+//           )}
+//         </ThemeContext.Consumer>
+//       </>
+//     );
+//   }
+// }
 
-// const App = () => {
-//   const { theme } = useContext(ThemeContext);
+const App = () => {
+  const { theme } = useContext(ThemeContext);
 
-//   const currentTheme = useMemo(() => {
-//     //themes[theme] = themes['dark'] = themes.theme = themes.dark
-//     return themes[theme] || themes.dark;
-//   }, [theme]);
+  const currentTheme = useMemo(() => {
+    //themes[theme] = themes['dark'] = themes.theme = themes.dark
+    return themes[theme] || themes.dark;
+  }, [theme]);
 
-//   return (
-//     <StyledThemeProvider theme={currentTheme}>
-//       <GlobalStyle />
-//       <Layout />
-//     </StyledThemeProvider>
-//   );
-// };
+  return (
+    <StyledThemeProvider theme={currentTheme}>
+      <GlobalStyle />
+      <Layout />
+    </StyledThemeProvider>
+  );
+};
 
 export default App;
--- src/contexts/ThemeContext.js
+++ src/contexts/ThemeContext.js
@@ -2,41 +2,73 @@ import React, { createContext, useState, useEffect } from "react";
 
 export const ThemeContext = createContext({});
 
-export function ThemeProvider({ children }) {
-  const [theme, setTheme] = useState("dark");
+export class ThemeProvider extends React.Component {
+  constructor(props) {
+    super(props);
 
-  function handleToggleTheme() {
-    if (theme === "light") {
-      localStorage.setItem("theme", "dark");
-      setTheme("dark");
-    } else {
-      localStorage.setItem("theme", "light");
-      setTheme("light");
+    let theme = "dark";
+    try {
+      theme = JSON.parse(localStorage.getItem("theme"));
+    } catch (error) {
+      console.log(error);
     }
+
+    this.state = {
+      theme
+    };
   }
 
-  // useEffect(() => {
-  //   const localTheme = localStorage.getItem("theme");
-  //   if (localTheme) {
-  //     setTheme(localTheme);
-  //   } else {
-  //     setTheme("dark");
-  //   }
-  // }, [theme]);
+  handleToggleTheme = () => {
+    this.setState(
+      prevState => ({
+        theme: prevState.theme === "dark" ? "light" : "dark"
+      }),
+      () => {
+        localStorage.setItem("theme", JSON.stringify(this.state.theme));
+      }
+    );
+  };
 
-  useEffect(() => {
-    const localTheme = localStorage.getItem("theme");
-
-    if (localTheme) {
-      setTheme(localTheme);
-    } else {
-      setTheme((prevState) => prevState);
-    }
-  }, [theme]);
-
-  return (
-    <ThemeContext.Provider value={{ theme, handleToggleTheme }}>
-      {children}
-    </ThemeContext.Provider>
-  );
+  render() {
+    return (
+      <ThemeContext.Provider
+        value={{
+          theme: this.state.theme,
+          handleToggleTheme: this.handleToggleTheme
+        }}
+      >
+        {this.props.children}
+      </ThemeContext.Provider>
+    );
+  }
 }
+
+// export function ThemeProvider({ children }) {
+//   const [theme, setTheme] = useState("dark");
+
+//   function handleToggleTheme() {
+//     if (theme === "light") {
+//       localStorage.setItem("theme", "dark");
+//       setTheme("dark");
+//     } else {
+//       localStorage.setItem("theme", "light");
+//       setTheme("light");
+//     }
+//   }
+
+//   useEffect(() => {
+//     const localTheme = localStorage.getItem("theme");
+
+//     if (localTheme) {
+//       setTheme(localTheme);
+//     } else {
+//       setTheme((prevState) => prevState);
+//     }
+//   }, [theme]);
+
+//   return (
+//     <ThemeContext.Provider value={{ theme, handleToggleTheme }}>
+//       {children}
+//     </ThemeContext.Provider>
+//   );
+// }
