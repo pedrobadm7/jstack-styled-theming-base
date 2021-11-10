37f8f65280a9386d459da9f517729171682e0d52
Pedro Barros <pedrobars7a@gmail.com>
2021-11-10 19:59:06 -0300

    HOC par amonitorar a context API

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -2,32 +2,48 @@ import React, { Component, useContext } from "react";
 import { ThemeContext } from "../../contexts/ThemeContext";
 import { Container } from "./styles";
 
-// export default class Header extends Component {
-//   render() {
-//     return (
-//       <ThemeContext.Consumer>
-//         {({ theme, handleToggleTheme }) => {
-//           <Container>
-//             <h1>JStack's Blog</h1>
-//             <button type="button" onClick={handleToggleTheme}>
-//               {theme === "dark" ? "🌞" : "🌚"}
-//             </button>
-//           </Container>;
-//         }}
-//       </ThemeContext.Consumer>
-//     );
-//   }
+function HOC(ComponenteHeader) {
+  return class Component extends React.Component {
+    render() {
+      return (
+        <ThemeContext.Consumer>
+          {value => <ComponenteHeader {...value} />}
+        </ThemeContext.Consumer>
+      );
+    }
+  };
+}
+
+class Header extends Component {
+  componentDidUpdate(prevProps, prevState) {
+    if (this.props.theme !== prevProps.theme) {
+      console.log("Tema mudou");
+    }
+  }
+
+  render() {
+    return (
+      <Container>
+        <h1>JStack's Blog</h1>
+        <button type="button" onClick={this.props.handleToggleTheme}>
+          {this.props.theme === "dark" ? "🌞" : "🌚"}
+        </button>
+      </Container>
+    );
+  }
+}
+
+// export default function Header() {
+//   const { theme, handleToggleTheme } = useContext(ThemeContext);
+
+//   return (
+//     <Container>
+//       <h1>JStack's Blog</h1>
+//       <button type="button" onClick={handleToggleTheme}>
+//         {theme === "dark" ? "🌞" : "🌚"}
+//       </button>
+//     </Container>
+//   );
 // }
 
-export default function Header() {
-  const { theme, handleToggleTheme } = useContext(ThemeContext);
-
-  return (
-    <Container>
-      <h1>JStack's Blog</h1>
-      <button type="button" onClick={handleToggleTheme}>
-        {theme === "dark" ? "🌞" : "🌚"}
-      </button>
-    </Container>
-  );
-}
+export default HOC(Header);
